b0a4bc54e5d53933eeed6b94f11393e984096e73
Anupam <anupampoudel78@gmail.com>
2024-12-02 19:37:51 +0545

    feature: added our services page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/sections/Hero";
+import Services from "@/sections/Services";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <Services />
     </main>
   );
 }
