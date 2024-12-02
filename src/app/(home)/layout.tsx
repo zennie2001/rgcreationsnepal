a7f83c9cebfea748a45c1d6ea5c2dedfb2ea4b85
Anupam <anupampoudel78@gmail.com>
2024-12-02 18:08:35 +0545

    modification: simple routing modification

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from 'next/font/google';
-import "./globals.css";
+import "../globals.css";
 import { twMerge } from "tailwind-merge";
 import Header from "@/sections/Header";
 
