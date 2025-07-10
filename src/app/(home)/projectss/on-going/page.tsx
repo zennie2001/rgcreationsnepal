3b6d6748884a5fdd3778f18b2dc31fc991d71b08
jennydev <jennydev2001@gmail.com>
2025-07-10 19:19:59 +0545

    projects

--- src/app/(home)/projectss/on-going/page.tsx
+++ src/app/(home)/projectss/on-going/page.tsx
@@ -6,7 +6,9 @@ const page = () => {
   return (
     <div className="overflow-hidden ">
        <ProjectOnGoingHero />
-        
+        <div className='py-8 h-svh text-center'>
+          Updating soon...
+        </div>
     </div>
   )
 }
