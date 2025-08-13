6e4b06ffd14bb31045eb4113e030a0e50e91b019
jennydev <jennydev2001@gmail.com>
2025-08-13 22:04:11 +0545

    Header

--- src/app/(home)/head.tsx
+++ src/app/(home)/head.tsx
@@ -1,9 +1,11 @@
 export default function Head() {
   return (
     <>
+    <link rel="canonical" href="https://www.rgcreationsnepal.com/" />
       {/* Required Meta Tags */}
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      
       
       {/* SEO Meta Tags */}
       <title> Best Architecture Company in Kathmandu, Nepal </title>
@@ -15,7 +17,7 @@ export default function Head() {
         name="keywords"
         content="Number 1 architect company in Nepal, Best architects in Nepal, Top architecture firms in Nepal, Best architecture company in Kathmandu, Award-winning architecture company in Nepal, Best hospitality architecture firm, RG Creations Nepal"
       />
-      <link rel="canonical" href="https://www.rgcreationsnepal.com/" />
+      
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -34,6 +34,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7TCGPVGPF8"></script>
+         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7TCGPVGPF8');
+            `,
+          }}
+        />
+      </head>
         
       
       <body className={twMerge(jakarta.variable, "antialiased font-sans overflow-y-scroll  scrollbar-visible ")}>
